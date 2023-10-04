79bfb1c6350d29d5353a30cd5daa6ca7c199f478
koki428 <koki.kikkawa@gmail.com>
2023-10-04 10:48:22 +0900

    カリキュラム8-3課題3まで

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use App\Http\Controllers\PostController;
 */
 
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
 
 //Route::get('/', function () {
 //    return view('welcome');
